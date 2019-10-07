e17ebff62d95acdf86e9744f18e5b60f05c81ab7
Castellani <bruno-castellani@hotmail.com>
2019-10-07 20:41:16 -0300

    Bot Basic Message Functions

--- branches/ServerfyBOT/index.js
+++ branches/ServerfyBOT/index.js
@@ -1,12 +1,13 @@
 const TelegramBot = require('./telegram');
 
 // replace the value below with the Telegram token you receive from @BotFather
+// ServerfyBOT token
 const token = '711197504:AAE9lt1B1K0o8x3Iu9PMISPAoAxCD-nmeTg';
 
 // Create a bot that uses 'polling' to fetch new updates
 const bot = new TelegramBot(token, {polling: true});
 
-// Matches "/echo [whatever]"
+// Matches "/echo [whatever]", used to get regex expressions
 bot.onText(/\/echo (.+)/, (msg, match) => {
   // 'msg' is the received Message from Telegram
   // 'match' is the result of executing the regexp above on the text content
@@ -19,11 +20,66 @@ bot.onText(/\/echo (.+)/, (msg, match) => {
   bot.sendMessage(chatId, resp);
 });
 
-// Listen for any kind of message. There are different kinds of
-// messages.
+// Listen for any kind of message. There are different kinds of messages.
 bot.on('message', (msg) => {
   const chatId = msg.chat.id;
 
   // send a message to the chat acknowledging receipt of their message
   bot.sendMessage(chatId, 'Received your message');
+});
+
+// Listen to text messages only
+bot.on('text', (msg) => {
+  const chatId = msg.chat.id;
+
+  // send a message to the chat acknowledging receipt of their message
+  bot.sendMessage(chatId, 'Received your text message');
+});
+
+// Listen to audio messages only
+bot.on('audio', (msg) => {
+  const chatId = msg.chat.id;
+
+  // send a message to the chat acknowledging receipt of their message
+  bot.sendMessage(chatId, 'Received your audio message');
+});
+
+// Listen to document messages only
+bot.on('document', (msg) => {
+  const chatId = msg.chat.id;
+
+  // send a message to the chat acknowledging receipt of their message
+  bot.sendMessage(chatId, 'Received your document message');
+});
+
+// Listen to photo messages only
+bot.on('photo', (msg) => {
+  const chatId = msg.chat.id;
+
+  // send a message to the chat acknowledging receipt of their message
+  bot.sendMessage(chatId, 'Received your photo message');
+});
+
+// Listen to sticker messages only
+bot.on('sticker', (msg) => {
+  const chatId = msg.chat.id;
+
+  // send a message to the chat acknowledging receipt of their message
+  bot.sendMessage(chatId, 'Received your sticker message');
+});
+
+// Listen to video messages only
+bot.on('video', (msg) => {
+  const chatId = msg.chat.id;
+
+  // send a message to the chat acknowledging receipt of their message
+  bot.sendMessage(chatId, 'Received your video message');
+});
+
+// Listen to voice messages only
+bot.on('voice', (msg) => {
+  const chatId = msg.chat.id;
+
+  // send a message to the chat acknowledging receipt of their message
+  bot.sendMessage(chatId, 'Received your voice message');
 });
